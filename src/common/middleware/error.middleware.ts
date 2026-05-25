14f20630df96c59f0300632019b67c55f3aff435
Ananya Verma <annyv566@gmail.com>
2026-05-25 20:30:56 +0530

    added logger for error to log errors on terminal

--- src/common/middleware/error.middleware.ts
+++ src/common/middleware/error.middleware.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import logger from "../logger/logger";
 
 export const errorHandler = (
   error: any,
@@ -6,6 +7,12 @@ export const errorHandler = (
   res: Response,
   _next: NextFunction,
 ) => {
+  logger.error({
+    message: error.message,
+    stack: error.stack,
+  },
+  "Unhandled application error"
+  );
   return res.status(400).json({
     success: false,
     message: error.message || "Something went Wrong",
